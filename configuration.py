c910271b8d02b96dd5693d39be42635ef0d730b2
rakeshroshan0398 <47413149+rakeshroshan0398@users.noreply.github.com>
2020-07-28 16:36:08 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,2 +1,10 @@
-input_folder = 'D:\\PG_Germany\\Input'
-output_folder = 'D:\\PG_Germany\\Output'
+#RR
+#input_folder = 'D:\\PG_Germany\\Input'
+#output_folder = 'D:\\PG_Germany\\Output'
+#AA
+#input_folder = 'D:\\PG_Germany\\Input'
+#output_folder = 'D:\\PG_Germany\\Output'
+#PG
+#input_folder = '\\\\blisscr016.bcc.pg.com\\Images\\PBQA\\Processed'
+input_folder = 'I:\\Images\\PBQA\\Processed'
+output_folder = "I:\\Images\\PBQA\\AffineNOK"
